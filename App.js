11c00f7a817ae41ee371a3fb67ce617efdbf4b12
Hiep Nguyen <hiepnh610@gmail.com>
2020-10-17 17:15:33 +0700

    setup redux saga for project

--- App.js
+++ App.js
@@ -9,14 +9,23 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import AsyncStorage from '@react-native-community/async-storage';
-import {createStore} from 'redux';
-import devToolsEnhancer from 'remote-redux-devtools';
+import {createStore, applyMiddleware} from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
+import {composeWithDevTools} from 'remote-redux-devtools';
 
 import Navigation from './src/navigations';
 import rootReducer from './src/store/reducers';
+import rootSaga from './src/store/saga';
 import ROOT_ACTIONS from './src/store/actions';
 
-const store = createStore(rootReducer, devToolsEnhancer());
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware, logger)),
+);
+
+sagaMiddleware.run(rootSaga);
 
 const App = () => {
   AsyncStorage.getItem('token', (token) => {
